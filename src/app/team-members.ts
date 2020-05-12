20494e752fdc0e225e377f905cd4b7d932076e1a
Erik19852404 <yorick_1985@mail.ru>
2020-05-12 16:26:13 +0600

    1 file modified

--- src/app/team-members.ts
+++ src/app/team-members.ts
@@ -1,7 +1,7 @@
 export const teamMembers = [
   {
     name: 'Apyshev Erik',
-    photo: 'https://cdn.jsdelivr.net/gh/Erik19852404/zya@master/src/assets/images/photo-default.png',
+    photo: 'https://cdn.jsdelivr.net/gh/Erik19852404/my-web-site@master/src/assets/images/photo1.png',
     photo_description: 'photo_1',
     about: 'Breaf information.',
     full_story: 'full story.',
