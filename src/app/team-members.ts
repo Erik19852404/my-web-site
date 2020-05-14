c32f6a7bb98d2e8ab627bbdc6d10a46f7a6e7400
Erik19852404 <yorick_1985@mail.ru>
2020-05-14 20:02:54 +0600

    5 files modified

--- src/app/team-members.ts
+++ src/app/team-members.ts
@@ -8,6 +8,6 @@ export const teamMembers = [
     ],
     photo_description: 'photo',
     about: 'I\'m 35 years old, maried and have a lovely daughter.',
-    full_story: 'I was born in 1985 in Kyrgizstan. I finished Bishkek Humanities University and got a degree of Specialist(bachelor) of sociology. In our country sociology was not very popular and didn\'t give good prospect for career. So, more then 7 years I worked in casino, because of high salary. But it wasn\'t interesting to me, I always wanted to take part in software development. And one nice day I got a chance to become participant of online game production with wonderful "Allods Team". It was my first step like a software tester...',
+    full_story: 'I was born in 1985 in Kyrgizstan. I finished Bishkek Humanities University and got a degree of Specialist(bachelor) of sociology. In our country sociology was not very popular and didn\'t give good prospect for career. So, more then 7 years I worked in casino, because of high salary. But it wasn\'t interesting to me, I always wanted to take part in software development. And one nice day I got a chance to become participant of online game production with wonderful "Allods Team". It was my first step like a software tester. Now I\'m professional tester with different useful skills. My english is not very good. I can write and read documentation without any problem, but I had in enough speaking practice, so my verbal skills isn\'t very good.',
   },
 ];
