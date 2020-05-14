45a789e5d5808f676669d304a0a3ef995c0b18d2
Erik19852404 <yorick_1985@mail.ru>
2020-05-14 18:24:18 +0600

    6 files modified

--- src/app/team-members.ts
+++ src/app/team-members.ts
@@ -6,8 +6,8 @@ export const teamMembers = [
       'https://cdn.jsdelivr.net/gh/Erik19852404/my-web-site@master/src/assets/images/photo2.jpg',
       'https://cdn.jsdelivr.net/gh/Erik19852404/my-web-site@master/src/assets/images/photo3.jpg'
     ],
-    photo_description: 'photo_1',
-    about: 'Breaf information.',
-    full_story: 'full story.',
+    photo_description: 'photo',
+    about: 'I\'m 35 years old, maried and have a lovely daughter.',
+    full_story: 'I was born in 1985 in Kyrgizstan. I finished Bishkek Humanities University and got a degree of Specialist(bachelor) of sociology. In our country sociology was not very popular and didn\'t give good prospect for career. So, more then 7 years I worked in casino, because of high salary. But it wasn\'t interesting to me, I always wanted to take part in software development. And one nice day I got a chance to become participant of online game production with wonderful "Allods Team". It was my first step like a software tester...',
   },
 ];
