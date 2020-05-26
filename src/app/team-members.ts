59cd0bef5023179d91780418bb7940933a9f4141
Erik19852404 <yorick_1985@mail.ru>
2020-05-26 08:26:54 +0600

    1 file modified

--- src/app/team-members.ts
+++ src/app/team-members.ts
@@ -7,7 +7,7 @@ export const teamMembers = [
       'https://cdn.jsdelivr.net/gh/Erik19852404/my-web-site@master/src/assets/images/photo3.jpg'
     ],
     photo_description: 'photo',
-    about: 'I\'m 35 years old, maкried and have a lovely daughter.',
-    full_story: 'I was born in 1985 in Kyrgizstan. I finished Bishkek Humanities University and got a degree of Specialist(bachelor) of sociology. In our country sociology was not very popular and didn\'t give good prospect for career. So, more then 7 years I worked in casino, because of high salary. But it wasn\'t interesting to me, I always wanted to take part in software development. And one nice day I got a chance to become participant of online game production with wonderful "Allods Team". It was my first step like a software tester. Now I\'m professional tester with different useful skills. My english is not very good. I can write and read documentation without any problem, but I hadn\'t enough speaking practice, so my verbal skills isn\'t very good.',
+    about: 'I\'m 35 years old, married and have a lovely daughter.',
+    full_story: 'I was born in 1985 in Kyrgizstan. I finished Bishkek Humanities University and got a degree of Specialist(bachelor) of sociology. In our country sociology was not very popular and I hadn\'t opportunity to make a good career. So, more then 7 years I worked in various casinos, because of high salary. But it wasn\'t interesting to me, I always wanted to take part in software development. And ones I got a chance to become a participant of online game production with the wonderful "Allods Team". It was the first step in a software tester career. Now I\'m professional tester with different useful skills. My level of english is intermadiate. I can write and read documentation without any problem, but I hadn\'t enough speaking practice.',
   },
 ];
